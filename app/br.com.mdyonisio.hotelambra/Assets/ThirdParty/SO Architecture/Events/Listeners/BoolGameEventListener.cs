63e81a33b68a384d2e3a770f157dff14164d5e3f
Mateus Dyonisio <mateeusdyonisio@gmail.com>
2020-11-17 23:49:11 -0300

    ADD: add SO Architeture

---
+++ app/br.com.mdyonisio.hotelambra/Assets/ThirdParty/SO Architecture/Events/Listeners/BoolGameEventListener.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjectArchitecture
+{
+    [AddComponentMenu(SOArchitecture_Utility.EVENT_LISTENER_SUBMENU + "bool Event Listener")]
+    public sealed class BoolGameEventListener : BaseGameEventListener<bool, BoolGameEvent, BoolUnityEvent>
+    {
+        public bool invertParameter;
+
+        protected override void RaiseResponse(bool value)
+        {
+            if (invertParameter)
+            {
+                base.RaiseResponse(!value);
+            }
+            else
+            {
+                base.RaiseResponse(value);
+            }
+        }
+
+    }
+}
